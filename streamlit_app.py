116105e308dea49e7ffde4942246dc752751c721
yuvaldror <yuval.dror@campus.technion.ac.il>
2024-07-15 15:33:01 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,5 +6,5 @@ import math
 # Set the title and favicon that appear in the Browser's tab bar.
 st.set_page_config(
     page_title='לנדוור חישוב טיפים',
-    page_icon=':U+2615:', # This is an emoji shortcode. Could be a URL too.
+    page_icon=':coffee:', # This is an emoji shortcode. Could be a URL too.
 )
