187919cb4b493967cec6f27d2ba08ed5d786eb3f
yuvaldror <yuval.dror@campus.technion.ac.il>
2024-07-15 15:36:25 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,3 +8,16 @@ st.set_page_config(
     page_title='לנדוור חישוב טיפים',
     page_icon=':coffee:', # This is an emoji shortcode. Could be a URL too.
 )
+with st.form(key='tips_form'):
+    # Input for the amount of tips collected
+    tips_amount = st.number_input('הכנס את סכום הטיפים שנאסף', min_value=0.0, step=0.01)
+    
+    # Input for the date of the shift
+    shift_date = st.date_input('בחר את תאריך המשמרת', value=datetime.today())
+
+    # Submit button
+    submit_button = st.form_submit_button(label='שלח')
+
+if submit_button:
+    # If the submit button is pressed, save the data
+    st.success(f'טיפים בסכום של {tips_amount} ש"ח שנאספו בתאריך {shift_date} נשמרו.')
